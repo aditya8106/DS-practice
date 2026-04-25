3b61579ed9dea2982d4c89d4fd92b6e241bb783a
adithya <adithyayegiti93@gmail.com>
2026-04-25 19:19:47 +0530

    two sum in normal way

--- arrays/twoSum.js
+++ arrays/twoSum.js
@@ -11,3 +11,17 @@ function twoSum(numbers, target) {
 }
 
 console.log(twoSum([2, 7, 11, 15], 9));
+
+ function twoSum2(){
+        const numbers = [2, 7, 11, 15];
+        const target = 9;
+        for (let i = 0; i < numbers.length; i++) {
+            for (let j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] === target) {
+                    return [i, j];
+                }
+            }        }
+        return null;
+ }
+
+
