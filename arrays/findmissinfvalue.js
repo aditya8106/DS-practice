dab8046625071a698ab8e1d007309d23f7ad00a4
adithya <adithyayegiti93@gmail.com>
2026-04-30 22:39:14 +0530

    approach 2 optimal one

--- arrays/findmissinfvalue.js
+++ arrays/findmissinfvalue.js
@@ -15,3 +15,17 @@ console.log(findMissingValue([0, 1, 2, 4, 5])); // Output: 3
 console.log(findMissingValue([0, 1, 3, 4, 5])); // Output: 2
 console.log(findMissingValue([1, 2, 3, 4, 5])); // Output: 0    
 console.log(findMissingValue([0, 1, 2, 3, 4])); // Output: 5
+
+///approach 2
+function findMissingValue2(arr) {
+    let n = arr.length + 1;
+    let total = n *(n-1)/2;
+    let sum = arr.reduce((a,b)=>a+b,0); // 0 means the initial value of the accumulator (a) before the first iteration. If the array is empty, it will return 0. If the array has elements, it will sum them up starting from 0.
+    return total - sum ;
+}
+
+console.log(findMissingValue2([0, 1, 2, 4, 5])); // Output: 3
+console.log(findMissingValue2([0, 1, 3, 4, 5])); // Output: 2
+console.log(findMissingValue2([1, 2, 3, 4, 5])); // Output: 0    
+console.log(findMissingValue2([0, 1, 2, 3, 4])); // Output: 5
+
