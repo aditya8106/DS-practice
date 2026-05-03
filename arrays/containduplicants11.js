5da6e1c3072e34e4086721ae9bb67a8e0975b3e5
adithya <adithyayegiti93@gmail.com>
2026-05-03 23:37:16 +0530

    co

--- arrays/containduplicants11.js
+++ arrays/containduplicants11.js
@@ -1,9 +1,18 @@
 /// contain duplicates in an array  sliding window approach
+// Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that arr[i] = arr[j] and the absolute difference between i and j is at most k.
+// Example 1:
+// Input: arr = [1,2,3,1], k = 3
+// Output: true
+// Explanation: The duplicate number is 1 and its indices are 0 and 3. The absolute difference between them is 3 which is equal to k.
+// Example 2:
+// Input: arr = [1,0,1,1], k = 1
+// Output: true
+// Explanation: The duplicate number is 1 and its indices are 0 and 2. The absolute difference between them is 2 which is greater than k.
 function containDuplicates(arr , k){     
     let set = new Set();
     let left = 0;    // left pointer of the sliding window
     for (let right = 0; right < arr.length; right++) {
-        if (right - left > k) {
+        if (right - left > k) { // right - left means the size of the current window previously we were checking if the size of the window exceeds k but now we are checking if the size of the window is greater than k because we want to maintain a window of size k
             set.delete(arr[left]);/// remove the leftmost element from the set when the window size exceeds k
             left++;// move the left pointer to the right to maintain the window size of k
         }
